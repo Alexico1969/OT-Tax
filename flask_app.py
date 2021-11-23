a7b3fd52ab12795d9b17b146fbcf6bda9e60eb2b
Alex van Winkel <alexicoo@gmail.com>
2021-11-23 09:45:40 -0500

    created tables and login form

--- flask_app.py
+++ flask_app.py
@@ -2,12 +2,15 @@
 from os import getenv, environ
 from flask import Flask, render_template, session, request, redirect, url_for, g
 from db import get_db_conn
+from helper import extract_username, extract_password
+from tables import create_tables
 
 
 app=Flask(__name__, static_url_path='/static')
 app.secret_key = 'Bruce Wayne is Spiderman'
 
-conn = get_db_conn
+conn = get_db_conn()
+create_tables(conn)
 
 @app.route('/')
 def home_page():
@@ -15,7 +18,20 @@ def home_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   pass
+
+    if request.method == "POST":
+        data = request.get_data("username")
+        print("data:", data)
+        converted = data.decode('UTF-8')
+        print("converted:", converted)
+        username = extract_username(converted)
+        print("username:", username)
+        password = extract_password(converted)
+        print("password:", password)
+        
+
+
+    return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
--- tables.py
+++ tables.py
@@ -3,3 +3,36 @@ from flask import g
 import psycopg2
 from urllib import parse
 
+def create_tables(conn):
+
+    # Create table users
+    query = '''create table if not exists users (
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            roles TEXT NOT NULL
+            )    '''
+    conn.execute(query)
+
+    # Create table mutations
+    query = '''create table if not exists mutations (
+            mutation_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            amount INTEGER,
+            date TEXT,
+            donor TEXT,
+            remarks TEXT
+            )    '''
+    conn.execute(query)
+
+    #create table monthly_data
+    query = '''create table if not exists monthly_data (
+                md_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            month TEXT,
+            goal INTEGER,
+            remarks TEXT
+            )    '''
+    conn.execute(query)
+            
+            
+    print("tables created where needed")
